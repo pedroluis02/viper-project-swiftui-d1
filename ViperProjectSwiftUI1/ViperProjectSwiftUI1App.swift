d34e573c23f25b40d347a41442822ce99a4094c6
Pedro Luis <pedroluisnr22@gmail.com>
2023-06-20 23:08:58 -0500

    Initial Commit

---
+++ ViperProjectSwiftUI1/ViperProjectSwiftUI1App.swift
@@ -0,0 +1,17 @@
+//
+//  ViperProjectSwiftUI1App.swift
+//  ViperProjectSwiftUI1
+//
+//  Created by Pedro Luis on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ViperProjectSwiftUI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
